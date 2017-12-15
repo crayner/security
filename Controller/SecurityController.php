c691c06b1fed70f4cbd77eb6885c46bda6b3bd7b
Craig Rayner <craig@craigrayner.com>
2017-12-15 13:20:47 +1100

    Routing now working

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -3,11 +3,16 @@ namespace HillRange\Security\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Class SecurityController
+ * @package HillRange\Security\Controller
+ */
 class SecurityController extends AbstractController
 {
-	public function login(Request $request)
+	public function loginAction(Request $request)
 	{
-
+		return new Response('Security stuff here!');
 	}
 }
--- DependencyInjection/HillRangeSecurityExtension.php
+++ DependencyInjection/HillRangeSecurityExtension.php
@@ -5,17 +5,20 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\Routing\RouteCollection;
 
 class HillRangeSecurityExtension extends Extension
 {
 	public function load(array $configs, ContainerBuilder $container)
 	{
+		$configuration = new Configuration();
+		$config        = $this->processConfiguration($configuration, $configs);
+
 		$locator = new FileLocator(__DIR__ . '/../Resources/config');
 		$loader = new YamlFileLoader(
 			$container,
 			$locator
 		);
 		$loader->load('services.yaml');
+
 	}
 }
