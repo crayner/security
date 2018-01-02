5c3418b0f8a882e5c0c53f180aca19ca9b0af13c
Craig Rayner <craig@craigrayner.com>
2018-01-02 15:32:39 +1100

    Restore Injection Scripts

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,16 @@
+<?php
+namespace Hillrange\Security\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+	public function getConfigTreeBuilder()
+	{
+		$treeBuilder = new TreeBuilder();
+		$rootNode = $treeBuilder->root('hill_range_security');
+
+		return $treeBuilder;
+	}
+}
