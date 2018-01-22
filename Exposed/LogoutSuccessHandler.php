a5cdb9ba9d2b2c6272cd93b66773316cfcb80d60
Craig Rayner <craig@craigrayner.com>
2018-01-22 17:37:39 +1100

    Disappearing Password Debug...

--- Exposed/LogoutSuccessHandler.php
+++ Exposed/LogoutSuccessHandler.php
@@ -2,8 +2,9 @@
 namespace Hillrange\Security\Exposed;
 
 use Hillrange\Security\Util\ParameterInjector;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
 
 class LogoutSuccessHandler implements LogoutSuccessHandlerInterface
@@ -16,22 +17,22 @@ class LogoutSuccessHandler implements LogoutSuccessHandlerInterface
 	/**
 	 * @var \Twig_Environment
 	 */
-	private $twig;
+	private $router;
 
 	/**
 	 * LogoutSuccessHandler constructor.
 	 *
 	 * @param ParameterInjector $parameterInjector
 	 */
-	public function __construct(ParameterInjector $parameterInjector, \Twig_Environment $twig)
+	public function __construct(ParameterInjector $parameterInjector, RouterInterface $router)
 	{
 		$this->parameterInjector = $parameterInjector;
-		$this->twig = $twig;
+		$this->router = $router;
 	}
 	/**
 	 * @param Request $request
 	 *
-	 * @return \Symfony\Component\HttpFoundation\Response|void
+	 * @return RedirectResponse
 	 */
 	public function onLogoutSuccess(Request $request)
 	{
@@ -43,6 +44,6 @@ class LogoutSuccessHandler implements LogoutSuccessHandlerInterface
 
 		$session->getFlashBag()->setAll($flash);
 
-		return new Response($this->twig->render('@HillrangeSecurity/security/logout.html.twig'));
+		return new RedirectResponse($this->router->generate($this->parameterInjector->getParameter('security.routes.security_home')));
 	}
 }
