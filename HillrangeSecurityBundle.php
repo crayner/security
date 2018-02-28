ca7b1557160ee219803fc795fe1769ceb0de352e
craigray <craig@craigrayner.com>
2018-02-28 11:09:27 +1100

    Force failure if routing not correctly figured.
    Signed-off-by: craigray <craig@craigrayner.com>

--- HillrangeSecurityBundle.php
+++ HillrangeSecurityBundle.php
@@ -1,6 +1,7 @@
 <?php
 namespace Hillrange\Security;
 
+use Hillrange\CKEditor\Exception\Exception;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -9,5 +10,10 @@ class HillrangeSecurityBundle extends Bundle
 	public function build(ContainerBuilder $container)
 	{
 		parent::build($container);
-	}
+
+		$dest = realpath('../config/routes/') . '/hillrange_security.yaml';
+
+		if (! file_exists($dest))
+		    throw new Exception('You must copy the routes.yaml file in the bundle Resource/config directory to the app config/routes directory as hillrange_security.yaml.');
+    }
 }
