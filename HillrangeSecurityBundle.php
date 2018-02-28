6340cc1fd85a85e1b0a47c6bebaa317aad7e1420
craigray <craig@craigrayner.com>
2018-02-28 11:30:26 +1100

    Debug Again
    Signed-off-by: craigray <craig@craigrayner.com>

--- HillrangeSecurityBundle.php
+++ HillrangeSecurityBundle.php
@@ -8,11 +8,11 @@ class HillrangeSecurityBundle extends Bundle
 {
 	public function build(ContainerBuilder $container)
 	{
-		parent::build($container);
-
 		$dest = realpath('../config/routes/') . '/hillrange_security.yaml';
 
 		if (! file_exists($dest))
 		    throw new \Exception('You must copy the routes.yaml file in the bundle Resource/config directory to the app config/routes directory as hillrange_security.yaml.');
+
+        parent::build($container);
     }
 }
--- Util/VersionManager.php
+++ Util/VersionManager.php
@@ -3,7 +3,7 @@ namespace Hillrange\Security\Util;
 
 class VersionManager
 {
-    const VERSION = '0.0.10';
+    const VERSION = '0.0.11';
 
     /**
      * @return string
