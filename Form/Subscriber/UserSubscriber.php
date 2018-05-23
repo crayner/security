b38b4b2751067f84ce1b0dd3a4d25ca68cef9261
craigray <craig@craigrayner.com>
2018-05-23 10:39:29 +1000

    Restore and repair toggle switch management on user form
    Signed-off-by: craigray <craig@craigrayner.com>

--- Form/Subscriber/UserSubscriber.php
+++ Form/Subscriber/UserSubscriber.php
@@ -30,6 +30,17 @@ class UserSubscriber implements EventSubscriberInterface
 		$data['usernameCanonical'] = trim($data['username']);
 		$data['emailCanonical']    = trim(strtolower($data['email']));
 
+        if (empty($data['enabled']))
+            $data['enabled'] = false;
+        if (empty($data['expired']))
+            $data['expired'] = false;
+        if (empty($data['credentials_expired']))
+            $data['credentials_expired'] = false;
+
+		$data['enabled'] = $data['enabled'] === '1' ? true : false;
+		$data['expired'] = $data['expired'] === '1' ? true : false;
+		$data['credentials_expired'] = $data['credentials_expired'] === '1' ? true : false;
+
 		$event->setData($data);
 	}
 }
--- Util/VersionManager.php
+++ Util/VersionManager.php
@@ -3,7 +3,7 @@ namespace Hillrange\Security\Util;
 
 class VersionManager
 {
-    const VERSION = '0.0.28';
+    const VERSION = '0.0.29';
 
     /**
      * @return string
